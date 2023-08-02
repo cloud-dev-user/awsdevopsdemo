4f62ae648d7f6f0871378fe3b2620bfbefc45d2f
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2023-08-02 11:57:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// This is a sample JavaScript file
+// You can add your custom JavaScript code here
+// For demonstration purposes, we'll just log a message to the console
+console.log("Sample JavaScript code executed.");
